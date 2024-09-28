776e039dc5748685a2d43e3695166bafd8d34c92
nirpal rawat <nirpalrawat@gmail.com>
2024-09-28 15:39:04 +0530

    rawat

--- controllers/FrontControllers.js
+++ controllers/FrontControllers.js
@@ -1,70 +1,58 @@
-const UserModel = require('../models/user')
+const UserModel = require("../models/user");
 
-class FrontController{
-    static home =async(req,res)=>{
-        try{
-            res.render("home")
-
-        }catch(error)
-        {
-            console.log(error)
-        }
+class FrontController {
+  static home = async (req, res) => {
+    try {
+      res.render("home");
+    } catch (error) {
+      console.log(error);
     }
+  };
 
-    static about =async(req,res)=>{
-        try{
-            res.render("about")
-
-        }catch(error)
-        {
-            console.log(error)
-        }
+  static about = async (req, res) => {
+    try {
+      res.render("about");
+    } catch (error) {
+      console.log(error);
     }
+  };
 
-    static login =async(req,res)=>{
-        try{
-            res.render("login")
-
-        }catch(error)
-        {
-            console.log(error)
-        }
+  static login = async (req, res) => {
+    try {
+      res.render("login");
+    } catch (error) {
+      console.log(error);
     }
-    static register =async(req,res)=>{
-        try{
-            res.render("register")
-
-        }catch(error)
-        {
-            console.log(error)
-        }
+  };
+  static register = async (req, res) => {
+    try {
+      res.render("register");
+    } catch (error) {
+      console.log(error);
     }
+  };
 
-    static contact =async(req,res)=>{
-        try{
-            res.render("contact")
-
-        }catch(error)
-        {
-            console.log(error)
-        }
+  static contact = async (req, res) => {
+    try {
+      res.render("contact");
+    } catch (error) {
+      console.log(error);
     }
-    static insertstudent =async(req,res)=>{
-        try{
-            //console.log(req.body)
-            const { name,email,password,confirmpassword} = req.body
-            const data = await UserModel.create({
-                name,
-                email,
-                password,
-            })
-            res.redirect('/'); //route path
-
-        }catch(error)
-        {
-            console.log(error)
-        }
+  };
+  static insertstudent = async (req, res) => {
+    try {
+      //console.log(req.body)
+      const { name, email, password, confirmpassword } = req.body;
+      const data = await UserModel.create({
+        name,
+        email,
+        password,
+      });
+      res.redirect("/"); //route path
+    } catch (error) {
+      console.log(error);
     }
+  };
 }
 
-module.exports = FrontController
+module.exports = FrontController;
